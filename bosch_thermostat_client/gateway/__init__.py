806be591188fd7a97c0abb4aa12bcdfb1c1cf964
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2021-04-13 09:58:22 +0200

    Fix gateway chooser

--- bosch_thermostat_client/gateway/__init__.py
+++ bosch_thermostat_client/gateway/__init__.py
@@ -2,7 +2,12 @@ from .ivt_gateway import IVTGateway
 from .nefit_gateway import NefitGateway
 from .easycontrol_gateway import EasycontrolGateway
 from bosch_thermostat_client.const.ivt import IVT
+from bosch_thermostat_client.const.nefit import NEFIT
+from bosch_thermostat_client.const.easycontrol import EASYCONTROL
 
 
 def gateway_chooser(device_type=IVT):
-    return IVTGateway if device_type == IVT else NefitGateway
+    return {IVT: IVTGateway,
+            NEFIT: NefitGateway,
+            EASYCONTROL: EasycontrolGateway
+            }[device_type]
